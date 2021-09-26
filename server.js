02ff57f331b45abdeb7ced2dfcb848c9474da483
Ven Kim <venvex@gmail.com>
2021-09-26 15:52:42 -0400

    first test

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
 const express = require("express");
 const path = require("path");
-
+//paths
 const controller = require("./controllers");
-
+//handlebars
 const exphbs = require("express-handlebars");
-
+//Sequelize
 const sequelize = require("./config/connection");
-
+//Session
 const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const SequlizeStore = require("connect-session-sequelize")(session.Store);
 
+//set up the actual session
 const sess = {
-  secret: "password",
+  secret: "super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  store: new SequelizeStore({
+  store: new SequlizeStore({
     db: sequelize,
   }),
 };
-
+//initialize the server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//middlewear
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
 
+//use controllers
 app.use("/", controller);
 
+//set handlebars as render engine
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
